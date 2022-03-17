a50eda146c5127c07212f073be7b07b5c73ccbf5
Naveen Kumar Veerapalli <naveenvicky770@gmail.com>
2022-03-17 18:07:52 +0530

    custom select layout and functionality without select tag

--- src/app/components/order-header/custom-select/custom-select.component.ts
+++ src/app/components/order-header/custom-select/custom-select.component.ts
@@ -11,7 +11,6 @@ import { DataCommuniationServiceService } from 'src/app/services/data-communiati
 })
 export class CustomSelectComponent implements OnInit {
   @Input() selectOf = '';
-  check="check"
   availableDeliveryStatuses = [
     'Placed',
     'In Transit',
@@ -20,36 +19,39 @@ export class CustomSelectComponent implements OnInit {
   ];
   availableLocations = ['Bangalore', 'Hyderabad', 'Patna'];
   currentOptions: string[] = [];
-  deliveryStatus = '';
-  locationStatus = '';
+  selectedOption='';
+  status = true;
   constructor(private dataComService: DataCommuniationServiceService) {}
 
   ngOnInit(): void {
     if (this.selectOf === 'status') {
-      this.currentOptions=this.availableDeliveryStatuses;
+      this.currentOptions = this.availableDeliveryStatuses;
     }
     if (this.selectOf === 'distribution') {
-      this.currentOptions=this.availableLocations;
+      this.currentOptions = this.availableLocations;
+    }
+    this.selectedOption=this.selectOf
+  }
+
+  changeOption(option: any, selectOf: any) {
+    this.selectedOption=option
+    this.status = !this.status;
+    if(option===selectOf){
+      option=""
+    }
+    if (selectOf === 'status') {
+      this.changeDeliveryStatus(option);
+    }
+    if (selectOf === 'distribution') {
+      this.changeLocationStatus(option);
     }
   }
 
-  changeOption(event: any, selectOf: any){
-    console.log(event,selectOf)
-    if(selectOf==='status'){
-      this.changeDeliveryStatus(event)
-    }
-    if(selectOf==='distribution'){
-      this.changeLocationStatus(event)
-    }
+  changeDeliveryStatus(optionSelected: any) {
+    this.dataComService.sendDeliveryFilterText(optionSelected);
   }
 
-  changeDeliveryStatus(event: any) {
-    this.deliveryStatus = event.target.value;
-    this.dataComService.sendDeliveryFilterText(this.deliveryStatus);
-  }
-
-  changeLocationStatus(event: any) {
-    this.locationStatus = event.target.value;
-    this.dataComService.sendLocationFilterText(this.locationStatus);
+  changeLocationStatus(optionSelected: any) {
+    this.dataComService.sendLocationFilterText(optionSelected);
   }
 }
